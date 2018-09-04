0a589613c550866aef66d011f9717b1186c54f45
rajeshb1987 <rajeshbommidi@gmail.com>
2018-09-04 21:06:09 +0530

    First commit in JUnit project

---
+++ src/test/HomePageTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class HomePageTest {
+	
+	@Test
+	public void validateLogin() throws Exception
+	{
+		System.out.println("Inside method validateLogin()");
+	}
+
+}
